cc841c6f9cb00ea67423300659723763076fc08c
mjgaughan <58380965+mjgaughan@users.noreply.github.com>
2022-01-20 13:23:15 -0500

    parser works, a bit ugly

--- h-parse.py
+++ h-parse.py
@@ -5,9 +5,10 @@ from string import punctuation
 #import sys
 import glob
 from typing import List, Optional, Tuple
+import csv
 #import re 
 #import attr
-C_PROHIB = ["return ", "=", "/*", "*/", "while ", ">", "if ", "define ", "extern "]
+C_PROHIB = ["return ", "=", "\n*", "*/", "while ", ">", "if ", "define ", "extern "]
 PUNCTUATION = [",", ";"]
 PROHIB_START = set(op[0] for op in C_PROHIB)
 
@@ -26,16 +27,24 @@ def h_to_txt(h_filename):
 def recurse_down(root_dir):
     print(root_dir)
     print(os.getcwd())
-    for filename in glob.glob(root_dir + '**/*.h', recursive=True):
-        print(filename)
-        parse_txt(filename)
+    with open('testing.csv', 'w') as csvfile:
+        csvwriter = csv.writer(csvfile) 
+        csvwriter.writerow(['func_prototype', 'line', 'file', 'in_macro']) 
+        for filename in glob.glob(root_dir + '**/*.h', recursive=True):
+            print("starting parse on " + filename)
+            parse_txt(filename, csvwriter)
+            print("done with " + filename)
     
 #parsing the h file as txt
-def parse_txt(txtfile):
+def parse_txt(txtfile, writer):
     with open(txtfile) as f:
         chars = f.read()
         block = ""
+        line_loc = 0
         for char in chars:
+            #print(char)
+            if char == "\n":
+                line_loc += 1
             #if # is the character, can skip
             if "#" in char:
                 continue
@@ -43,16 +52,21 @@ def parse_txt(txtfile):
             block += char
             
             #this has to be all of the logic for parsing 
-            if char == '}' or char == ";":
+            if char == '}' or char == ";" or list(block)[-2:] == ['\n', '\n']:
                 if "{" in block:
                     block = block[:block.find('{')]
                 valid = True
+                if "*/" in block:
+                        block = block[block.find('*/') + 2:]
                 for entry in C_PROHIB:
                     if entry in block:
                         valid = False
                 #checks form of lines to see if they match function prototypes as defined in literature
-                valid = check_form(block, valid)
-                if '(' in block and ')' in block:
+                if ('(' in block and ')' in block):
+                    valid = check_form(block, valid)
+                    #print("----")
+                    #print(block)
+                    #print(valid)
                     #isolating arguments between first open paren and last close paren
                     arguments = block[block.find('(') + 1 :block.rfind(')') - 1]
                     temp_arg = ""
@@ -75,15 +89,17 @@ def parse_txt(txtfile):
                         if "(" in cleaned_arg and check_form(cleaned_arg, True):
                             if cleaned_arg[1] == "(":
                                 cleaned_arg = cleaned_arg[2:]
-                            print("---------")
-                            print(cleaned_arg)
-                            print("---------")
+                            #print("---------")
+                            #print(cleaned_arg + " is located at line " + str(line_loc) + " in file " + str(txtfile))
+                            writer.writerow([cleaned_arg, line_loc, str(txtfile), "True"])
+                            #print("---------")
                     #this block above does the function prototype within thing
                     if valid:
-                        print("---------")
+                        #print("---------")
                         func_prototype = block
-                        print(func_prototype)
-                        print("---------")
+                        #print(func_prototype + " is located at line " + str(line_loc) + " in file " + str(txtfile))
+                        writer.writerow([func_prototype, line_loc, str(txtfile), "False"])
+                        #print("---------")
                 block = ""
             #previous_char = char
             
@@ -96,11 +112,19 @@ def check_form(block, valid):
     if begin_args == -1:
         return False
     prev_char = '('
+    #this should discrim against MACROS
+    seen_space = False
     for index in range(len(block)):
         #iterating backwards, beginning at the theoretical argument intake
         current_char = block[begin_args - index]
         #if there is a space and the preceding char is alphanumeric than it should satisfy
-        
+        if current_char == " ":
+            if prev_char == "(":
+                return False
+            seen_space = True
+        #this should discrim against MACROS which have similar structure to function prototype but lack 
+        if begin_args - index == 0 and not seen_space:
+            return False
         if prev_char == " " and current_char.isalnum() and current_char not in PUNCTUATION:
             return True
         #if there are any commas or semicolons preceding the parentheses then it is not an argument intake
