3b6cb651757dea002f2c54c33c4670d807fdf255
mgaughan <mgaughan@middlebury.edu>
2022-01-30 18:11:05 -0500

    found locations of func/prot; need editing/git rollback

--- hard_check.py
+++ hard_check.py
@@ -6,23 +6,34 @@ def main(csv_reader, csv_writer):
     location = 0
     for row in csv_reader:
         if location != 0:
-            print(row)
             func_name = row[0][:row[0].index('(')]
             func_name = func_name[func_name.rfind(' ') + 1:]
-            print(func_name)
             #TODO look for opening gzip and finding file locations from there
-            find_declaration_location(func_name)
+            loc_locations = find_location(func_name)
+            if len(loc_locations) < 2:
+                print('invalid function/prototype')
+                continue
+            else:
+                print(row)
+                print(loc_locations)
         location += 1
         if location > 10:
             break
 
-def find_declaration_location(func_name):
+def find_location(func_name):
+    locations = {}
     with open("tags_f_p.csv") as tags:
         tags_reader = csv.reader(tags, delimiter = ',')
         for row in tags_reader:
-            print(row)
+            #print(row)
             if row[0] == func_name:
-                print(row)
+                if row[3] == 'f':
+                    print(row)
+                    locations['function'] = row
+                else:
+                    print(row)
+                    locations['prototype'] = row
+    return locations
 
 
 def gz_to_csv(gzip_file, csv_writer):
@@ -34,10 +45,18 @@ def gz_to_csv(gzip_file, csv_writer):
             if len(array_line) > 3 and (array_line[3] == 'f' or array_line[3] == 'p'):
                 csv_writer.writerow(array_line)
 
+
+def check_csv(filename):
+    with open(filename) as csvs:
+        reader = csv.reader(csvs, delimiter = ',')
+        for row in reader:
+            print(row)
+
 if  __name__ == "__main__":
     print("bozo")
-    with open("final_full.csv") as read, open("tags_f_p.csv", "w") as write:
+    with open("final_full.csv") as read, open("temp.csv", "w") as write:
         csv_read = csv.reader(read, delimiter=',')
         csv_write = csv.writer(write)
-        gz_to_csv("tags.gz", csv_write)
-        #main(csv_read, csv_write)
+        #gz_to_csv("tags.gz", csv_write)
+        main(csv_read, csv_write)
+        #check_csv("tags_f_p.csv") 
