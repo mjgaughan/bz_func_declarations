83ff036af5bde93600b412c059adba34962cf85c
mgaughan <mgaughan@middlebury.edu>
2022-02-02 15:01:37 -0500

    updating commands parse

---
+++ parse_commands.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+def find_func(filename, target_file):
+    with open(filename) as commands:
+        lines = commands.readlines()
+        for line in lines:
+            #print("-----")
+            #print(line)
+            if target_file in line:
+                temp = ""
+                index = 0
+                valid = False
+                for char in line:
+                    temp += char
+                    if char == ";":
+                        index += 1
+                        if index == 2 and  "CC" in temp:
+                            valid = True
+                        if index == 3 and valid and "gcc" in temp:
+                            return temp[:temp.index(";")]
+                        temp = ""
+
+
+if __name__ == "__main__":
+    print("hi")
+    print(find_func("commands.sh", "arch/x86/events/amd/uncore.c "))
